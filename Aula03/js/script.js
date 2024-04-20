9763fb661265a922e10e05a5c21be15c9460c6a8
Edilson Silva <edilson@yahoo.com.br>
2024-04-20 11:58:41 -0300

    Aula 3 - If else e for

---
+++ Aula03/js/script.js
@@ -0,0 +1,10 @@
+numero = prompt("Digite um número");
+// Vamos calcular o numero digitado em busca do resto da divisão
+// se o resto for igual a 0(zero), então o número digitado é Par
+// caso contrário, será Impar
+if(numero % 2 == 0){
+    alert("O número digitado é Par");
+}
+else{
+    alert("O número digitado é Impar");
+}
